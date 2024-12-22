71367dabed930c004b6e3042359e8b0dbceaca58
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2024-12-22 20:13:39 +0100

    Update 1480: Running Sum of 1d Array
    Use itertools.accumulate() for a cleaner solution without modifying the
    input array.

--- solutions/1480-running-sum-of-1d-array.py
+++ solutions/1480-running-sum-of-1d-array.py
@@ -11,10 +11,9 @@ https://leetcode.com/problems/running-sum-of-1d-array/
 [3, 4, 6, 16, 17]
 """
 
+from itertools import accumulate
+
 
 class Solution:
     def runningSum(self, nums: list[int]) -> list[int]:
-        for i in range(1, len(nums)):
-            nums[i] += nums[i - 1]
-
-        return nums
+        return list(accumulate(nums))
