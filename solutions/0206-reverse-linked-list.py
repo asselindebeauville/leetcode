a07283638eca92caf3dcdc923660a014f6d50347
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2025-01-16 13:20:21 +0100

    Add 206: Reverse Linked List

---
+++ solutions/0206-reverse-linked-list.py
@@ -0,0 +1,35 @@
+"""206. Reverse Linked List
+
+https://leetcode.com/problems/reverse-linked-list/
+
+>>> solution = Solution()
+>>> solution.reverseList([1, 2, 3, 4, 5])  # doctest: +SKIP
+[5, 4, 3, 2, 1]
+>>> solution.reverseList([1, 2])  # doctest: +SKIP
+[2, 1]
+>>> solution.reverseList([])  # doctest: +SKIP
+[]
+"""
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        current = head
+
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+
+        return prev
