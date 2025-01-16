014db725abd6f25a030abead262d22cfe1b3a6f6
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2025-01-16 14:52:40 +0100

    Add 24: Swap Nodes in Pairs

---
+++ solutions/0024-swap-nodes-in-pairs.py
@@ -0,0 +1,42 @@
+"""24. Swap Nodes in Pairs
+
+https://leetcode.com/problems/swap-nodes-in-pairs/
+
+>>> solution = Solution()
+>>> solution.swapPairs([1, 2, 3, 4])  # doctest: +SKIP
+[2, 1, 4, 3]
+>>> solution.swapPairs([])  # doctest: +SKIP
+[]
+>>> solution.swapPairs([1])  # doctest: +SKIP
+[1]
+>>> solution.swapPairs([1, 2, 3])  # doctest: +SKIP
+[2, 1, 3]
+"""
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = dummy = ListNode(0, head)
+        current = head
+
+        while current and current.next:
+            first = current
+            second = current.next
+
+            prev.next = second
+            first.next = second.next
+            second.next = first
+
+            prev = first
+            current = first.next
+
+        return dummy.next
