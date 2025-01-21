a047268724fac8f29267a345dff133aa5f76461e
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2025-01-21 18:28:36 +0100

    Add 234: Palindrome Linked List

---
+++ solutions/0234-palindrome-linked-list.py
@@ -0,0 +1,48 @@
+"""234. Palindrome Linked List
+
+https://leetcode.com/problems/palindrome-linked-list/
+
+>>> solution = Solution()
+>>> # head = [1, 2, 2, 1]
+>>> head = ListNode(1, ListNode(2, ListNode(2, ListNode(1))))
+>>> solution.isPalindrome(head)
+True
+>>> # head = [1, 2]
+>>> head = ListNode(1, ListNode(2))
+>>> solution.isPalindrome(head)
+False
+"""
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        prev = None
+        slow = fast = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+
+            next_node = slow.next
+            slow.next = prev
+            prev = slow
+            slow = next_node
+
+        if fast:
+            slow = slow.next
+
+        while slow:
+            if slow.val != prev.val:
+                return False
+            prev = prev.next
+            slow = slow.next
+
+        return True
