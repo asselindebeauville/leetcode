f77004ee91501c1298be2e6d344c70d497497acb
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2025-01-24 17:26:42 +0100

    Add 933: Number of Recent Calls

---
+++ solutions/0933-number-of-recent-calls.py
@@ -0,0 +1,34 @@
+"""933. Number of Recent Calls
+
+https://leetcode.com/problems/number-of-recent-calls/
+
+>>> obj = RecentCounter()
+>>> obj.ping(1)
+1
+>>> obj.ping(100)
+2
+>>> obj.ping(3001)
+3
+>>> obj.ping(3002)
+3
+"""
+
+from collections import deque
+
+
+class RecentCounter:
+    def __init__(self):
+        self.queue = deque()
+
+    def ping(self, t: int) -> int:
+        self.queue.append(t)
+
+        while self.queue[0] < t - 3000:
+            self.queue.popleft()
+
+        return len(self.queue)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
